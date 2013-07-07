3b73d026dff3626723d283765fca77a6a466d6ab
michel.vidailhet <michel.vidailhet@gmail.com>
2013-07-07 10:02:32 +0200

    more changes

--- index.php
+++ index.php
@@ -2,72 +2,120 @@
 <html>
 
 <head>
+    <title>Dédiciné</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.0/jquery.js" type="text/javascript"></script>
-    <link rel="stylesheet" href="styles/general.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Mitch">
+
+    <link href="application/assets/css/beforeBootstrapStyle.css" rel="stylesheet">
+    <link href="application/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="application/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="application/assets/css/general.css" rel="stylesheet">
+
 </head>
 
 <body>
 
-<?php
-include_once("sources/functions.php");
-?>
+    <?php
+        include_once("application/sources/functions.php");
+        $rootDirectories = getRootDirectories();
+    ?>
 
-<div id="header">
-    <span class="v-aligner"></span><h1 class="verticalCenter">Welcome to Dediciné !</h1>
-</div>
-
-<div id="main">
-
-    <div class="block bigFolder first">
-        <a href="parsedirectory.php?path=./films" class="title"><span class="v-aligner"></span><span class="verticalCenter">Films</span></a>
-        <?php
-        $littleList = true;
-        $currentDirectory = "./films";
-        include("views/filelist.php");
-        ?>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar-inner">
+            <div class="container-fluid">
+                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <p class="navbar-text pull-right">
+                    Espace restant : <?php echo getRemainingDiskSpace(); ?>
+                </p>
+                <a class="brand" href="/">Dédiciné</a>
+                <div class="nav-collapse collapse">
+                    <ul class="nav">
+                        <li class="active"><a href="/">Home</a></li>
+                        <?php foreach($rootDirectories as $folderName => $name) { ?>
+                            <li><a href="<?php echo "parsedirectory.php?path=./".$folderName; ?>"><?php echo $name; ?></a></li>
+                        <?php } ?>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
     </div>
 
-    <div class="block bigFolder">
-        <a href="parsedirectory.php?path=./series" class="title"><span class="v-aligner"></span><span class="verticalCenter">Séries</span></a>
-        <?php
-        $littleList = true;
-        $currentDirectory = "./series";
-        include("views/filelist.php");
-        ?>
-    </div>
+    <div class="container-fluid">
+        <div class="row-fluid">
 
-    <div class="block bigFolder">
-        <a href="parsedirectory.php?path=./apps" class="title"><span class="v-aligner"></span><span class="verticalCenter">Apps</span></a>
-        <?php
-        $littleList = true;
-        $currentDirectory = "./apps";
-        include("views/filelist.php");
-        ?>
-    </div>
+            <div class="span3">
+                <div class="block">
+                    <a href="parsedirectory.php?path=./films" class="title text-center">Films</a>
+                    <?php
+                    $littleList = true;
+                    $currentDirectory = "./films";
+                    include("application/views/filelist.php");
+                    ?>
+                </div>
+            </div>
+            <div class="span3">
+                <div class="block">
+                    <a href="parsedirectory.php?path=./series" class="title text-center">Séries</a>
+                    <?php
+                    $littleList = true;
+                    $currentDirectory = "./series";
+                    include("application/views/filelist.php");
+                    ?>
+                </div>
+            </div>
+            <div class="span3">
+                <div class="block">
+                    <a href="parsedirectory.php?path=./films" class="title text-center">Musique</a>
+                    <?php
+                    $littleList = true;
+                    $currentDirectory = "./musique";
+                    include("application/views/filelist.php");
+                    ?>
+                </div>
+            </div>
+            <div class="span3">
+                <div class="block">
+                    <a href="parsedirectory.php?path=./films" class="title text-center">Apps</a>
+                    <?php
+                    $littleList = true;
+                    $currentDirectory = "./apps";
+                    include("application/views/filelist.php");
+                    ?>
+                </div>
+            </div>
+        </div>
 
-    <div class="block bigFolder last">
-        <a href="parsedirectory.php?path=./musique" class="title"><span class="v-aligner"></span><span class="verticalCenter">Musique</span></a>
-        <?php
-        $littleList = true;
-        $currentDirectory = "./musique";
-        include("views/filelist.php");
-        ?>
-    </div>
+        <div class="row-fluid">
+            <div class="span12">
+                <div class="block">
+                    <div class="title text-center">
+                        Autres fichiers
+                    </div>
+                    <?php
+                    $littleList = false;
+                    $currentDirectory = ".";
+                    include("application/views/filelist.php");
+                    ?>
+                </div>
+            </div>
+        </div>
 
-    <div class="clear"></div>
+        <hr>
 
-    <div class="bigList">
-        <h2>Autres fichiers</h2>
-        <?php
-        $littleList = false;
-        $currentDirectory = ".";
-        include("views/filelist.php");
-        ?>
-    </div>
+        <footer>
+            <p>&copy; Dédiciné 2013</p>
+        </footer>
 
-</div>
-
-
+    </div><!--/.fluid-container-->
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="application/bootstrap/js/bootstrap.js"></script>
 </body>
+
+<body>
 </html>
--- parsedirectory.php
+++ parsedirectory.php
@@ -2,25 +2,58 @@
 <html>
 
 <head>
+    <title>Dédiciné</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.0/jquery.js" type="text/javascript"></script>
-    <link rel="stylesheet" href="styles/general.css">
-</head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
+    <style type="text/css">
+        body {
+            padding-top: 60px;
+            padding-bottom: 40px;
+        }
+
+        .sidebar-nav {
+            padding: 9px 0;
+        }
+    </style>
+
+    <link href="application/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="application/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="application/assets/css/general.css" rel="stylesheet">
+
+</head>
 <body>
 
-<?php
-include_once("sources/functions.php");
-?>
+    <?php
+        include_once("application/sources/functions.php");
+        $rootDirectories = getRootDirectories();
+        $currentDirectory = $_GET["path"];
+    ?>
 
-<div id="header">
-    <span class="v-aligner"></span><h1 class="verticalCenter">Welcome to Dediciné !</h1>
-</div>
-
-<div id="main">
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar-inner">
+            <div class="container-fluid">
+                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="brand" href="/">Dédiciné</a>
+                <div class="nav-collapse collapse">
+                    <ul class="nav">
+                        <li><a href="/">Home</a></li>
+                        <?php foreach($rootDirectories as $folderName => $name) { ?>
+                        <li <?php if("./" . $folderName == $currentDirectory) echo 'class="active"'; ?>><a href="<?php echo "parsedirectory.php?path=./".$folderName; ?>"><?php echo $name; ?></a></li>
+                        <?php } ?>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+    </div>
 
     <?php
-        $currentDirectory = $_GET["path"];
         $tmpArr = explode("/", $currentDirectory);
         $arrayLength = count($tmpArr);
         $currentDirectoryName = $tmpArr[$arrayLength - 1];
@@ -45,19 +78,31 @@ include_once("sources/functions.php");
 
     ?>
 
-    <div class="bigList">
-        <h2>
-            <a href="<?php echo $backLink; ?>" class="backButton"><div class="icon backWhite"></div></a>
-            <?php echo $currentDirectoryName; ?>
-        </h2>
-        <?php
-        $files = getDirectoryList($currentDirectory);
-        include("views/filelist.php");
-        ?>
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span12">
+                <div class="block">
+                    <div class="title text-center">
+                        <a href="<?php echo $backLink; ?>" class="backButton"><div class="icon backWhite"></div></a>
+                        <?php echo $currentDirectoryName; ?>
+                    </div>
+                    <?php
+                    include("application/views/filelist.php");
+                    ?>
+                </div>
+            </div>
+        </div>
     </div>
 
-</div>
+    <hr>
 
+    <footer>
+        <p>&copy; Dédiciné 2013</p>
+    </footer>
+
+    </div><!--/.fluid-container-->
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="application/bootstrap/js/bootstrap.js"></script>
 
 </body>
 </html>
